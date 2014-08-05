9e01b8c0a1900aa21a1136dd75747f17e0153f1e
Taiki Sugawara <buzz.taiki@gmail.com>
2014-08-06 04:07:48 +0900

    remove unused imports

--- src/test/java/com/github/buzztaiki/jdk8039776/TargetVersionTest.java
+++ src/test/java/com/github/buzztaiki/jdk8039776/TargetVersionTest.java
@@ -5,13 +5,7 @@ import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Properties;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
 
 
 public class TargetVersionTest {
